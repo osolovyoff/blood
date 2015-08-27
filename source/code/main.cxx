ebce3140f2b76d9e66f8da31d1e72ba4573b1ed8
oleg solovyov <solovyov.cxx@gmail.com>
2015-08-27 23:25:25 +0400

    addition external submodules

--- source/code/main.cxx
+++ source/code/main.cxx
@@ -1,18 +1,4 @@
-#include <blood/chrono/timer.hxx>
-#include <blood/common/macros.hxx>
-#include <string>
-#include <iostream>
-
 int main()
 {
-    std::cout << "hello world" << blood_folder_separtor << std::endl;
-    std::cout << "hello world" << blood_folder_separtor << std::endl;
-    std::cout << "hello world" << blood_folder_separtor << std::endl;
-    std::cout << "hello world" << blood_folder_separtor << std::endl;
-    std::cout << "hello world" << blood_folder_separtor << std::endl;
-    std::cout << "hello world" << blood_folder_separtor << std::endl;
-    std::cout << "hello world" << blood_folder_separtor << std::endl;
-    std::cout << "hello world" << blood_folder_separtor << std::endl;
-    std::cin.get();
 	return 0;
 }
